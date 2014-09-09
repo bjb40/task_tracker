249536d1f0816d0578a719291f0aacb9ee9fd431
bjb40 <bryce_bartlett@yahoo.com>
2014-09-09 09:27:24 -0400

    Revise report to fix typos

--- report.py
+++ report.py
@@ -19,13 +19,13 @@ def makeplan(month):
     Input a month to create a print-out of the month.
     '''
     start_date = date(2014, month, 1)
-    end_date = date(2014, month, 31)
+    end_date = date(2014, month, 31) #need to write break because not all months have 31 days
 
     delta = end_date - start_date
 
     plan = open(plandir + str(month) + '-2014.csv', 'a')
 
-    plan.write('day, weekday')
+    plan.write('day,weekday')
 
     for i in range(delta.days + 1):
         print('writing '+ (start_date + td(days=i)).strftime('%m-%d') )
@@ -44,8 +44,9 @@ import pandas
 
 #>> Read in the data #
 
+repmonth = '08'
 pdat = pandas.read_csv(datref)
-daysdat = pandas.read_csv(plandir + 'plan-7-2014.csv')
+daysdat = pandas.read_csv(plandir + 'plan-' + repmonth + '-2014.csv')
 
 # set counter for workdays
 workdays = 0
@@ -67,7 +68,7 @@ project_mins = {}
 
 for i in range(0,len(pdat)):
  #   print pdat['date'][i]
-    if str(pdat['date'][i])[1:3] == '07':
+    if str(pdat['date'][i])[0:2] == repmonth:
         worked += float(pdat['min_spent'][i])
 
         if pdat['project'][i] in project_mins:
@@ -81,16 +82,16 @@ for i in range(0,len(pdat)):
 
 #>> call pandoc and build a .pdf or .html or .docx document for printing#
 
-reported = plandir + '07-report.md'
+reported = plandir + repmonth + '-report.md'
 
 makerep = open(reported, 'w')
 
 #yaml header
-makerep.write('---\nauthor: Bryce Bartlett\ndate: August 2014 \ntitle: July Report\n---\n\n')
+makerep.write('---\nauthor: Bryce Bartlett\ndate: September 2014 \ntitle: August Report\n---\n\n')
 
 #output for monthly overview
 makerep.write('#Overview\n\n')
-makerep.write('Planned for ' + str(workdays) + ' days of work in July.\n\n')
+makerep.write('Planned for ' + str(workdays) + ' days of work in August.\n\n')
 makerep.write('Worked a total of ' + str(round(worked/60.,3)) + ' hours, ')
 makerep.write('which amounts to an average of ' + str(round(worked/(float(workdays)*60.),2)) + ' hours per work day.\n\n\n')
 
@@ -104,8 +105,8 @@ for i in project_mins:
     makerep.write('|----------|-------------------------------------|-------|\n')
     
     for j in range(0,len(pdat)):
-        if pdat['project'][j] == i and str(pdat['date'][j])[1:3] == '07':
-            makerep.write('| ' + str(pdat['date'][j][4:6]) + ' | ' + str(pdat['task'][j]) + ' | ' + str(round(pdat['min_spent'][j]/60.,2)) + '|\n')
+        if pdat['project'][j] == i and str(pdat['date'][j])[0:2] == repmonth:
+            makerep.write('| ' + str(pdat['date'][j][3:5]) + ' | ' + str(pdat['task'][j]) + ' | ' + str(round(pdat['min_spent'][j]/60.,2)) + '|\n')
 
 makerep.close()
 
@@ -113,9 +114,9 @@ makerep.close()
 
 import subprocess
 
-fileout = os.path.splitext(reported)[0] + '.html'
+fileout = os.path.splitext(reported)[0] + '.docx'
 
-args =  ['pandoc', reported, '-o', fileout, '-s', '-S']
+args =  ['pandoc', reported, '-o', fileout, '-s']
 
 print(args)
 
